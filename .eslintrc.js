4b9fb542bb3a9da4fc89a38f68e58c0ff0a8a229
Patrick Lauher <plauher14@gmail.com>
2023-04-13 09:33:13 -0500

    Add Storybook integration on new branch
    Storybook "detected that your project does not have a babel
    configuration (.babelrc, babel.config.js, etc.) and ran the
    'missing-babelrc' migration.
    Storybook:
    - added the React preset
    - added Storybook plugin to .eslintrc.js
    - ran eslintPlugin migration

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,47 +2,42 @@ module.exports = {
   env: {
     node: true,
     es2022: true,
-    browser: true,
+    browser: true
   },
-  extends: ['eslint:recommended', 'plugin:astro/recommended', 'plugin:astro/jsx-a11y-strict'],
+  extends: ['eslint:recommended', 'plugin:astro/recommended', 'plugin:astro/jsx-a11y-strict', 'plugin:storybook/recommended'],
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module',
+    sourceType: 'module'
   },
   rules: {},
-  overrides: [
-    {
-      files: ['*.js'],
-      rules: {
-        'no-mixed-spaces-and-tabs': ['error', 'smart-tabs'],
-      },
-    },
-    {
-      files: ['*.astro'],
-      parser: 'astro-eslint-parser',
-      parserOptions: {
-        parser: '@typescript-eslint/parser',
-        extraFileExtensions: ['.astro'],
-      },
-      rules: {
-        'no-mixed-spaces-and-tabs': ['error', 'smart-tabs'],
-      },
-    },
-    {
-      files: ['*.ts'],
+  overrides: [{
+    files: ['*.js'],
+    rules: {
+      'no-mixed-spaces-and-tabs': ['error', 'smart-tabs']
+    }
+  }, {
+    files: ['*.astro'],
+    parser: 'astro-eslint-parser',
+    parserOptions: {
       parser: '@typescript-eslint/parser',
-      extends: ['plugin:@typescript-eslint/recommended'],
-      rules: {
-        '@typescript-eslint/no-unused-vars': [
-          'error',
-          { argsIgnorePattern: '^_', destructuredArrayIgnorePattern: '^_' },
-        ],
-        '@typescript-eslint/no-non-null-assertion': 'off',
-      },
+      extraFileExtensions: ['.astro']
     },
-    {
-      files: ['**/*.astro/*.js', '*.astro/*.js'],
-      parser: '@typescript-eslint/parser',
-    },
-  ],
-}
+    rules: {
+      'no-mixed-spaces-and-tabs': ['error', 'smart-tabs']
+    }
+  }, {
+    files: ['*.ts'],
+    parser: '@typescript-eslint/parser',
+    extends: ['plugin:@typescript-eslint/recommended'],
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['error', {
+        argsIgnorePattern: '^_',
+        destructuredArrayIgnorePattern: '^_'
+      }],
+      '@typescript-eslint/no-non-null-assertion': 'off'
+    }
+  }, {
+    files: ['**/*.astro/*.js', '*.astro/*.js'],
+    parser: '@typescript-eslint/parser'
+  }]
+};
